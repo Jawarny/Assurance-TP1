dff7342a0210bfdd4e68930ee27b12026cacfcac
Jawarny <Jawarny@hotmail.fr>
2020-02-09 21:40:31 -0500

    Creation du projet!

---
+++ TP1-Assurance/src/CV.java
@@ -0,0 +1,19 @@
+
+public class CV {
+	
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int nbAnneeTravail;
+	private String[] competences;
+	private String attentes;
+
+	public static void main( String[] args ) {
+		// TODO Auto-generated method stub
+		System.out.println( "Bienvenue chez Barette!" );
+	}
+	
+	public static void affiche() {
+		
+	}
+}

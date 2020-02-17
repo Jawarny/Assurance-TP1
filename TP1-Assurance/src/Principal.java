5e4ac9af975210c4d013463beef63aa6acdcc486
Jawarny <Jawarny@hotmail.fr>
2020-02-17 09:18:20 -0500

    testings

--- TP1-Assurance/src/Principal.java
+++ TP1-Assurance/src/Principal.java
@@ -8,16 +8,15 @@ public class Principal {
 	public static void main( String[] args ) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		
-		System.out.println( OutilsConstante.MESSAGE_BIENVENU );
-		String fichier = OutilsFichier.lireFichier( OutilsConstante.CHEMIN_FACTURE03 );
+		//System.out.println( OutilsConstante.MESSAGE_BIENVENU );
+		String fichier = OutilsFichier.lireFichier( OutilsConstante.CHEMIN_FACTURE );
 		
 		if ( !OutilsFichier.fichierRestaurent( fichier ) ) {
 			System.out.println( OutilsConstante.ERREUR_FICHIER );
-			OutilsFichier.sauvegarderFichier( OutilsConstante.CHEMIN_SFACTURE03, OutilsConstante.ERREUR_FICHIER );
+			OutilsFichier.sauvegarderFichier( OutilsConstante.CHEMIN_SFACTURE, OutilsConstante.ERREUR_FICHIER );
 		} else {
 			Restaurent res01 = new Restaurent ( fichier );
-			res01.afficherDetailler();
-			res01.afficherFacture( OutilsConstante.CHEMIN_SFACTURE03 );
+			res01.afficherFacture( OutilsConstante.CHEMIN_SFACTURE );
 		}
 		
 		String[] competencesElias = { "C#", "Java", "Lua" };
@@ -45,9 +44,6 @@ public class Principal {
 //		EliasJawhari.affiche();
 //		JosephKhalil.affiche();
 //		yasserSoliman.affiche();
-		
-
-		
 		
 		
 	}
